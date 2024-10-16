4738546c8260a0a083b7023672219ded3b336401
Matheus Coelho Pires <matheus.coelhopires@gmail.com>
2024-10-16 09:26:36 -0300

    feat: adicionadas mais rotas a aplicaçãoe, configurando o banco de dados e adicinado variaveis de ambiente

--- scr/server.js
+++ scr/server.js
@@ -1,1 +1,18 @@
+//Criando servidor e estabelecendo suas rotas...
 import {fastify} from 'fastify';
+
+const server = fastify();
+
+//Rotas Usuários 
+server.post('/users/register');
+
+server.get('/users/login');
+
+server.put('/users/profile:id');
+
+//Rotas Livros
+server.post('/books');
+
+server.get('/books');
+
+server.put('/books/:id');
